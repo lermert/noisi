included frequency domain, fixed bugs, tested gradient

noisi_v1/my_classes/noisesource.py
@@ -5,8 +5,6 @@ try:
 except ImportError:
     print('Plotting unavailable, is basemap installed?')
 
-from noisi_v1.util.geo import get_spherical_surface_elements
-
 
 class NoiseSource(object):
 
@@ -50,7 +48,7 @@ class NoiseSource(object):
 
         # return one spectrum in location with index iloc
         return np.dot(self.distr_basis[iloc, :],
-                      self.spect_basis) * self.surf_area[iloc]
+                      self.spect_basis)
 
     def plot(self, **options):
 

noisi_v1/my_classes/wavefield.py
@@ -17,18 +17,12 @@ from warnings import warn
 class WaveField(object):
     """
     Object to handle database of stored wavefields.
-    Basically, just a few methods to work on wavefields
-    stored in an hdf5 file.
-    The stored seismograms have to have sampling rate as attribute Fs and
-    number of time steps as attribute ntime; They have to have an ID of format
-    net.sta.loc.cha
+    Methods to work on wavefields stored in an hdf5 file.
     """
 
-    def __init__(self, file, sourcegrid=None, fdomain=False, w='r'):
+    def __init__(self, file, sourcegrid=None, w='r'):
 
         self.w = w
-        self.fdomain = fdomain
-        self.complex = complex
 
         try:
             self.file = h5py.File(file, self.w)
@@ -37,16 +31,21 @@ class WaveField(object):
             raise IOError(msg)
 
         self.stats = dict(self.file['stats'].attrs)
+        self.fdomain = self.stats['fdomain']
         self.sourcegrid = self.file['sourcegrid']
 
         self.data = self.file['data']
+
+        if self.fdomain:
+            self.freq = np.fft.rfftfreq(self.stats['npad'],
+                                        d=1. / self.stats['Fs'])
 
     def get_green(self, ix=None, by_index=True):
 
         if by_index:
             return(self.data[ix, :])
         else:
-            raise NotImplementedError("Not implemented.")
+            raise NotImplementedError("Not yet implemented.")
 
     def copy_setup(self, newfile, nt=None,
                    ntraces=None, w='r+'):
@@ -81,6 +80,9 @@ class WaveField(object):
 
     def truncate(self, newfile, truncate_after_seconds):
 
+        if self.fdomain:
+            raise NotImplementedError()
+
         nt_new = int(round(truncate_after_seconds * self.stats['Fs']))
         with self.copy_setup(newfile, nt=nt_new) as wf:
             for i in range(self.stats['ntraces']):
@@ -94,6 +96,9 @@ class WaveField(object):
 
     def filter_all(self, type, overwrite=False, zerophase=True,
                    outfile=None, **kwargs):
+
+        if self.fdomain:
+            raise NotImplementedError('Not implemented yet, filter beforehand')
 
         if type == 'bandpass':
             sos = filter.bandpass(df=self.stats['Fs'], **kwargs)
@@ -132,6 +137,8 @@ reopen to read / modify.' % newfile.file.filename)
         """
         Decimate the wavefield and save to a new file
         """
+        if self.fdomain:
+            raise NotImplementedError()
 
         fs_old = self.stats['Fs']
         freq = self.stats['Fs'] * 0.4 / float(decimation_factor)
@@ -167,9 +174,25 @@ resetting to last sample.')
             t_sample = np.shape(self.data)[1] - 1
 
         if resolution == 1:
-            snapshot = self.data[:, t_sample]
+            if self.fdomain:
+                snapshot = np.zeros(self.stats['ntraces'])
+                for i in range(self.stats['ntraces']):
+                    snapshot[i] = np.trapz(self.data[i, :] *
+                                           np.exp(self.freq *
+                                           np.pi * 2. * 1.j * t_sample),
+                                           dx=self.stats['Fs']).real
+            else:
+                snapshot = self.data[:, t_sample]
         else:
-            snapshot = self.data[0::resolution, t_sample]
+            if self.fdomain:
+                snapshot = np.zeros(self.data[0::resolution, 0].shape)
+                for i in range(len(snapshot)):
+                    snapshot[i] = np.trapz(self.data[i, :] *
+                                           np.exp(self.freq *
+                                           np.pi * 2. * 1.j * t_sample),
+                                           dx=1. / self.stats['Fs']).real
+            else:
+                snapshot = self.data[0::resolution, t_sample]
         return snapshot
 
     def plot_snapshot(self, t, resolution=1, **kwargs):

noisi_v1/scripts/correlation.py
@@ -4,7 +4,6 @@ import os
 import yaml
 from glob import glob
 from math import ceil
-from scipy.fftpack import next_fast_len
 from obspy import Trace
 from warnings import warn
 from noisi_v1 import NoiseSource, WaveField
@@ -18,9 +17,9 @@ try:
 except ImportError:
     pass
 # prepare embarrassingly parallel run:
-    comm = MPI.COMM_WORLD
-    size = comm.Get_size()
-    rank = comm.Get_rank()
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
 
 
 class config_params(object):
@@ -193,10 +192,10 @@ def get_ns(all_conf, insta=False):
         with WaveField(any_wavefield) as wf1:
             nt = int(wf1.stats['nt'])
             Fs = round(wf1.stats['Fs'], 8)
-
-    # Necessary length of zero padding
-    # for carrying out frequency domain correlations/convolutions
-    n = next_fast_len(2 * nt - 1)
+            n = wf1.stats['npad']
+    # # Necessary length of zero padding
+    # # for carrying out frequency domain correlations/convolutions
+    # n = next_fast_len(2 * nt - 1)
 
     # Number of time steps for synthetic correlation
     n_lag = int(all_conf.source_config['max_lag'] * Fs)
@@ -284,15 +283,20 @@ def compute_correlation(input_files, all_conf, nsrc, all_ns, taper,
                                     dt=1. / Fs)[0].data * taper
             s1 = np.ascontiguousarray(s1)
             s2 = np.ascontiguousarray(s2)
+            spec1 = np.fft.rfft(s1, n)
+            spec2 = np.fft.rfft(s2, n)
 
         else:
-            # read Green's functions
-            s1 = np.ascontiguousarray(wf1.data[i, :] * taper)
-            s2 = np.ascontiguousarray(wf2.data[i, :] * taper)
-
-        # Fourier transform for greater ease of convolution
-        spec1 = np.fft.rfft(s1, n)
-        spec2 = np.fft.rfft(s2, n)
+            if not wf1.fdomain:
+                # read Green's functions
+                s1 = np.ascontiguousarray(wf1.data[i, :] * taper)
+                s2 = np.ascontiguousarray(wf2.data[i, :] * taper)
+                # Fourier transform for greater ease of convolution
+                spec1 = np.fft.rfft(s1, n)
+                spec2 = np.fft.rfft(s2, n)
+            else:
+                spec1 = np.ascontiguousarray(wf1.data[i, :])
+                spec2 = np.ascontiguousarray(wf2.data[i, :])
 
         # convolve G1G2
         g1g2_tr = np.multiply(np.conjugate(spec1), spec2)

noisi_v1/scripts/kernel.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 from mpi4py import MPI
 import numpy as np
 import os
-import yaml
 from glob import glob
 from math import ceil
 
@@ -20,9 +19,9 @@ except ImportError:
     pass
 
 # prepare embarrassingly parallel run:
-    comm = MPI.COMM_WORLD
-    size = comm.Get_size()
-    rank = comm.Get_rank()
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
 
 
 def add_input_files(kp, all_conf, insta=False):
@@ -178,22 +177,27 @@ def compute_kernel(input_files, all_conf, nsrc, all_ns, taper,
             s2 = db.get_seismograms(source=fsrc, receiver=rec2,
                                     dt=dt)[0].data * taper
             s2 = np.ascontiguousarray(s2)
+            spec1 = np.fft.rfft(s1, n)
+            spec2 = np.fft.rfft(s2, n)
 
         else:
-            s1 = np.ascontiguousarray(wf1.data[i, :] * taper)
-            s2 = np.ascontiguousarray(wf2.data[i, :] * taper)
-
-        spec1 = np.fft.rfft(s1, n)
-        spec2 = np.fft.rfft(s2, n)
+            if not wf1.fdomain:
+                s1 = np.ascontiguousarray(wf1.data[i, :] * taper)
+                s2 = np.ascontiguousarray(wf2.data[i, :] * taper)
+                spec1 = np.fft.rfft(s1, n)
+                spec2 = np.fft.rfft(s2, n)
+            else:
+                spec1 = wf1.data[i, :]
+                spec2 = wf2.data[i, :]
 
         g1g2_tr = np.multiply(np.conjugate(spec1), spec2)
+        # spectrum -- to be replaced by spectral basis function
         c = np.multiply(g1g2_tr, S)
-
     #######################################################################
     # Get Kernel at that location
     #######################################################################
         corr_temp = my_centered(np.fft.ifftshift(np.fft.irfft(c, n)),
-                                n_corr)
+                                n_corr) * nsrc.surf_area[i]
 
     #######################################################################
     # Apply the 'adjoint source'
@@ -210,7 +214,6 @@ def compute_kernel(input_files, all_conf, nsrc, all_ns, taper,
 
         if i % print_each_n == 0 and all_conf.config['verbose']:
             print("Finished {} of {} source locations.".format(i, ntraces))
-
     if not insta:
         wf1.file.close()
         wf2.file.close()
@@ -281,8 +284,7 @@ def run_kern(args):
     # Current model for the noise source
     nsrc = os.path.join(all_conf.source_config['project_path'],
                         all_conf.source_config['source_name'],
-                        'iteration_' + str(all_conf.step),
-                        'starting_model.h5')
+                        'spectral_model.h5')
 
     # Smart numbers
     all_ns = get_ns(all_conf)  # ntime, n, n_corr, Fs

noisi_v1/scripts/run_correlation.py
@@ -200,13 +200,15 @@ def g1g2_corr(wf1, wf2, corr_file, src, source_conf, insta=False):
                 s2 = np.ascontiguousarray(s2)
 
             else:
-                # read Green's functions
-                s1 = np.ascontiguousarray(wf1.data[i, :] * taper)
-                s2 = np.ascontiguousarray(wf2.data[i, :] * taper)
-
-            # Fourier transform for greater ease of convolution
-            spec1 = np.fft.rfft(s1, n)
-            spec2 = np.fft.rfft(s2, n)
+                if not wf1.fdomain:
+                    # read Green's functions
+                    s1 = np.ascontiguousarray(wf1.data[i, :] * taper)
+                    s2 = np.ascontiguousarray(wf2.data[i, :] * taper)
+                    # Fourier transform for greater ease of convolution
+                    spec1 = np.fft.rfft(s1, n)
+                    spec2 = np.fft.rfft(s2, n)
+                else:
+                    pass
 
             # convolve G1G2
             g1g2_tr = np.multiply(np.conjugate(spec1), spec2)

noisi_v1/scripts/run_measurement.py
@@ -167,8 +167,8 @@ def measurement(source_config, mtype, step, ignore_net,
                 snr_a = snratio(tr_o, **_options_ac)
                 info.extend([msr_s, np.nan, msr, np.nan,
                              l2_so, snr, snr_a, tr_o.stats.sac.user0])
-                adjt *= (msr - msr_o)
-                adjoint_source[0].data += adjt
+                adjt *= (msr_s - msr_o)
+                adjoint_source[0].data = adjt
 
         measurements.loc[i] = info
 

noisi_v1/scripts/run_sourcesetup.py
@@ -6,7 +6,6 @@ import time
 from glob import glob
 import os
 import io
-from scipy.fftpack import next_fast_len
 import errno
 from noisi_v1 import WaveField
 from noisi_v1.util.geo import is_land, geographical_distances
@@ -125,6 +124,7 @@ and measr_config.yml to source model directory, please edit and rerun.")
         # load the source locations of the grid
         grd = np.load(os.path.join(conf['project_path'],
                                    'sourcegrid.npy'))
+
         # add the approximate spherical surface elements
         if grd.shape[-1] < 50000:
             surf_el = get_spherical_surface_elements(grd[0], grd[1])
@@ -133,18 +133,16 @@ and measr_config.yml to source model directory, please edit and rerun.")
 approximate surface elements.')
             surf_el = np.ones(grd.shape[-1]) * conf['grid_dx'] ** 2
 
+        # get the relevant array sizes
         wfs = glob(os.path.join(conf['project_path'], 'greens', '*.h5'))
         if wfs != [] and conf['verbose']:
             print('Found wavefield.')
         else:
             raise FileNotFoundError('No wavefield database found. Run \
 precompute_wavefield first.')
-
         with WaveField(wfs[0]) as wf:
             df = wf.stats['Fs']
-            nt = wf.stats['nt']
-
-        n = next_fast_len(2 * nt - 1)
+            n = wf.stats['npad']
         freq = np.fft.rfftfreq(n, d=1. / df)
         n_distr = len(parameter_sets)
         coeffs = np.zeros((grd.shape[-1], n_distr))
@@ -155,8 +153,7 @@ precompute_wavefield first.')
             coeffs[:, i] = self.distribution_from_parameters(grd,
                                                              parameter_sets[i],
                                                              conf['verbose'])
-            if not parameter_sets[i]['distribution'] == 'gaussian_blob':
-                coeffs[:, i] /= surf_el
+
             # plot
             outfile = os.path.join(args.source_model,
                                    'source_starting_model_distr%g.png' % i)
@@ -183,11 +180,26 @@ precompute_wavefield first.')
                                     'starting_model.h5'), 'w') as fh:
             fh.create_dataset('coordinates', data=grd)
             fh.create_dataset('frequencies', data=freq)
-            fh.create_dataset('model', data=coeffs.astype(np.float32))
+            fh.create_dataset('model', data=coeffs.astype(np.float))
             fh.create_dataset('spectral_basis',
-                              data=spectra.astype(np.float32))
+                              data=spectra.astype(np.float))
             fh.create_dataset('surface_areas',
-                              data=surf_el.astype(np.float32))
+                              data=surf_el.astype(np.float))
+
+        uniform_spatial = np.ones(coeffs.shape)
+        for ix_distr in range(n_distr):
+            uniform_spatial[:, ix_distr] *= float(parameter_sets[i]['weight'])
+
+        # Save to an hdf5 file
+        with h5py.File(os.path.join(args.source_model,
+                                    'spectral_model.h5'), 'w') as fh:
+            fh.create_dataset('coordinates', data=grd)
+            fh.create_dataset('frequencies', data=freq)
+            fh.create_dataset('model', data=uniform_spatial.astype(np.float))
+            fh.create_dataset('spectral_basis',
+                              data=spectra.astype(np.float))
+            fh.create_dataset('surface_areas',
+                              data=surf_el.astype(np.float))
 
     def distribution_from_parameters(self, grd, parameters, verbose=False):
 
@@ -195,15 +207,13 @@ precompute_wavefield first.')
             if verbose:
                 print('homogeneous distribution')
             distribution = np.ones(grd.shape[-1])
-            distribution /= grd.shape[-1]
-            return(distribution)
+            return(parameters['weight'] * distribution)
 
         elif parameters['distribution'] == 'ocean':
             if verbose:
                 print('ocean-only distribution')
             is_ocean = np.abs(is_land(grd[0], grd[1]) - 1.)
-            distribution = is_ocean / is_ocean.sum()
-            return(distribution)
+            return(parameters['weight'] * is_ocean)
 
         elif parameters['distribution'] == 'gaussian_blob':
             if verbose:
@@ -220,9 +230,8 @@ precompute_wavefield first.')
             if parameters['only_in_the_ocean']:
                 is_ocean = np.abs(is_land(grd[0], grd[1]) - 1.)
                 blob *= is_ocean
-                blob *= (grd.shape[-1] / is_ocean.sum())
 
-            return(blob)
+            return(parameters['weight'] * blob)
 
     def spectrum_from_parameters(self, freq, parameters):
 
@@ -232,6 +241,5 @@ precompute_wavefield first.')
         spec = taper * np.exp(-((freq - mu) ** 2) /
                               (2 * sig ** 2))
         spec = spec / (sig * sqrt(2. * pi))
-        spec *= parameters['weight']
 
         return(spec)

noisi_v1/scripts/run_wavefieldprep.py
@@ -5,10 +5,11 @@ from pandas import read_csv
 from warnings import warn
 import numpy as np
 from math import pi
-from scipy.signal import hann
 import h5py
 from obspy.geodetics import gps2dist_azimuth
 from noisi_v1 import WaveField
+from scipy.fftpack import next_fast_len
+from scipy.signal import lfilter, butter
 from noisi_v1.util.geo import geograph_to_geocent
 from glob import glob
 from mpi4py import MPI
@@ -38,7 +39,7 @@ class precomp_wavefield(object):
         self.config = config
 
         if self.config['wavefield_type'] == 'instaseis'\
-           and 'instaseis' not in locals():
+           and not 'instaseis' in globals():
             raise ImportError('Module instaseis was not found.')
 
         # create output folder
@@ -87,27 +88,50 @@ class precomp_wavefield(object):
 
         # initialize parameters
         self.Fs = self.config['wavefield_sampling_rate']
-        self.npts = int(self.config['wavefield_duration'] / self.Fs)
+        self.npts = int(self.config['wavefield_duration'] * self.Fs)
         self.ntraces = self.sourcegrid.shape[-1]
         self.data_quantity = self.config['synt_data']
-        self.freq = np.fft.rfftfreq(2 * self.npts, d=1.0 / self.Fs)
 
-        # Apply a freq. domain taper to suppress high and low frequencies.
-        freq_max = self.Fs / 2.0  # Nyquist
-        if freq_max < self.config['wavefield_filter'][1]:
-            warn("Selected upper filter corner is above Nyquist.\
- It will be reset.")
-        freq_min = 1. / self.config['wavefield_duration']  # lowest resolved
-        freq_max = min(freq_max, self.config['wavefield_filter'][1])
-        freq_min = min(freq_min, self.config['wavefield_filter'][0])
-        self.filt = [freq_min, freq_max]
+        if self.config['wavefield_domain'] == 'fourier':
+            self.fdomain = True
+            self.npad = 2 * self.npts - 2
+        elif self.config['wavefield_domain'] == 'time':
+            self.fdomain = False
+            self.npad = next_fast_len(2 * self.npts - 1)
+        else:
+            raise ValueError('Unknown domain {}.'.format(self.config
+                                                         ['wavefield_domain']))
+        self.freq = np.fft.rfftfreq(self.npad, d=1.0 / self.Fs)
 
+        # Apply a filter
+        if self.config['wavefield_filter'] is not None:
+            freq_nyq = self.Fs / 2.0  # Nyquist
+            if freq_nyq < self.config['wavefield_filter'][1]:
+                warn("Selected upper freq reset to Nyquist freq (was above).")
+            freq_minres = 1. / self.config['wavefield_duration']
+            # lowest resolved
+            freq_max = min(freq_nyq, self.config['wavefield_filter'][1])
+            freq_min = max(freq_minres, self.config['wavefield_filter'][0])
+
+            f0 = freq_min / freq_nyq
+            f1 = freq_max / freq_nyq
+            self.filter = butter(4, [f0, f1], 'bandpass')
+        else:
+            self.filter = None
+
+        # if using instaseis: Find and open database
         if self.config['wavefield_type'] == 'instaseis':
             path_to_db = self.config['wavefield_path']
             self.db = instaseis.open_db(path_to_db)
             if self.db.info['length'] < self.npts / self.Fs:
                 warn("Resetting wavefield duration to axisem database length.")
-                self.npts = self.db.info['length'] * self.Fs
+                fsrc = instaseis.ForceSource(latitude=0.0,
+                                             longitude=0.0, f_r=1.0)
+                rec = instaseis.Receiver(latitude=0.0, longitude=0.0)
+                test = self.db.get_seismograms(source=fsrc,
+                                               receiver=rec,
+                                               dt=1. / self.Fs)
+                self.npts = test[0].stats.npts
 
     def green_from_instaseis(self, station):
 
@@ -143,13 +167,23 @@ class precomp_wavefield(object):
             stats.attrs['ntraces'] = self.ntraces
             stats.attrs['Fs'] = self.Fs
             stats.attrs['nt'] = int(self.npts)
+            stats.attrs['npad'] = self.npad
+            if self.fdomain:
+                stats.attrs['fdomain'] = True
+            else:
+                stats.attrs['fdomain'] = False
 
             # DATASET NR 2: Source grid
             f.create_dataset('sourcegrid', data=self.sourcegrid)
 
             # DATASET Nr 3: Seismograms itself
-            traces = f.create_dataset('data', (self.ntraces, self.npts),
-                                      dtype=np.float32)
+            if self.fdomain:
+                traces = f.create_dataset('data', (self.ntraces,
+                                                   self.npts + 1),
+                                          dtype=np.complex64)
+            else:
+                traces = f.create_dataset('data', (self.ntraces, self.npts),
+                                          dtype=np.float32)
 
             for i in range(self.ntraces):
                 if i % 1000 == 0 and i > 0 and self.config['verbose']:
@@ -164,6 +198,7 @@ class precomp_wavefield(object):
                     values = self.db.get_seismograms(source=fsrc,
                                                      receiver=rec,
                                                      dt=1. / self.Fs)
+
                 elif self.config['synt_data'] == 'VEL':
                     values = self.db.get_seismograms(source=fsrc,
                                                      receiver=rec,
@@ -178,7 +213,18 @@ class precomp_wavefield(object):
                     raise ValueError('Unknown data quantity. \
 Choose DIS, VEL or ACC in configuration.')
 
-                traces[i, :] = values[c_index][0: self.npts]
+                if self.filter is not None:
+                    trace = lfilter(*self.filter, values[c_index].data)
+                else:
+                    trace = values[c_index].data
+
+                if self.fdomain:
+                    trace_fd = np.fft.rfft(trace[0: self.npts],
+                                           n=self.npad)
+                    traces[i, :] = trace_fd
+                else:
+                    traces[i, :] = trace[0: self.npts]
+        return()
 
     def green_spec_analytic(self, distance_in_m):
 
@@ -186,7 +232,7 @@ Choose DIS, VEL or ACC in configuration.')
         q = self.args.q
         rho = self.args.rho
         freq = self.freq
-        w = 2 * pi * freq
+        w = 2. * pi * freq
         g_fd = np.zeros(freq.shape, dtype=np.complex)
         f = float(self.config['wavefield_point_force'])
 
@@ -217,7 +263,7 @@ Choose DIS, VEL or ACC in configuration.')
         # initialize the file
         f_out = os.path.join(self.wf_path, station_id + '.h5')
 
-        with h5py.File(f_out, "w") as f:
+        with h5py.File(f_out, "w", ) as f:
 
             # DATASET NR 1: STATS
             stats = f.create_dataset('stats', data=(0,))
@@ -226,13 +272,22 @@ Choose DIS, VEL or ACC in configuration.')
             stats.attrs['ntraces'] = self.ntraces
             stats.attrs['Fs'] = self.Fs
             stats.attrs['nt'] = int(self.npts)
+            stats.attrs['npad'] = self.npad
+            if self.fdomain:
+                stats.attrs['fdomain'] = True
+            else:
+                stats.attrs['fdomain'] = False
 
             # DATASET NR 2: Source grid
             f.create_dataset('sourcegrid', data=self.sourcegrid)
 
             # DATASET Nr 3: Seismograms itself
-            f.create_dataset('data', (self.ntraces, self.npts),
-                             dtype=np.float32)
+            if self.fdomain:
+                f.create_dataset('data', (self.ntraces, self.npts),
+                                 dtype=np.complex)
+            else:
+                f.create_dataset('data', (self.ntraces, self.npts),
+                                 dtype=np.float)
 
             # loop over source locations
             for i in range(self.ntraces):
@@ -242,18 +297,19 @@ Choose DIS, VEL or ACC in configuration.')
 
                 g_fd = self.green_spec_analytic(r)
 
-                # apply the freq. domain taper
-                taper = np.zeros(self.freq.shape)
-                i0 = np.argmin(np.abs(self.freq - self.filt[0]))
-                i1 = np.argmin(np.abs(self.freq - self.filt[1]))
-                taper[i0: i1] = hann(i1 - i0)
+                # transform back to time
+                s = np.fft.irfft(g_fd, n=self.npad)[0: self.npts]
 
-            # transform back to time domain
-                g1_td_taper = np.fft.irfft(taper * g_fd)[0: self.npts]
+                # apply a filter if asked for
+                if self.filter is not None:
+                        trace = lfilter(*self.filter, s)
 
-            # write the result
-                f['data'][i, :] = g1_td_taper
-                f.flush()
+                if self.fdomain:
+                    f['data'][i, :] = np.fft.rfft(trace, n=self.npad)
+                else:
+                    f['data'][i, :] = trace
+                # flush
+                    f.flush()
 
             # close output file
             f.close()

noisi_v1/test/check_adjstf.py
@@ -9,7 +9,7 @@ from noisi_v1.scripts import adjnt_functs as af
 # input:
 # *********************************************
 steps = np.arange(-14, 0, 0.1)
-mtype = 'windowed_waveform'
+mtype = 'ln_energy_ratio'
 sacdict = {'dist': 1e6}
 g_speed = 3700.
 window_params = {}